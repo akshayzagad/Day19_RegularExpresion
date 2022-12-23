f0b913d49baf0347ef2663b3386501f13d31cdd7
akshayzagad <akshayzagade62@gmail.com.com>
2022-12-23 18:59:56 +0530

    Added;-First Name Test

--- src/RegularExpression.java
+++ src/RegularExpression.java
@@ -2,7 +2,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegularExpression {
-    public static void main(String[] args) {
-        System.out.println("Welcome to  RegularExpression programme");
+        static Pattern pattern = Pattern.compile("^([A-Z]?[a-z]{3,})$");
+        public static void main(String[] args)
+        {
+            Matcher matcher = pattern.matcher("Zagade");
+            if(matcher.matches())
+                System.out.println(true);
+            else
+                System.out.println(false);
+        }
     }
-}
+
