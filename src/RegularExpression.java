dbb5772f1f881c9841b72361416fa1c7ebfa337f
akshayzagad <akshayzagade62@gmail.com.com>
2022-12-23 20:35:23 +0530

    Added;- Test Password Rule 2 And add atleast 1 Upper Case

--- src/RegularExpression.java
+++ src/RegularExpression.java
@@ -2,10 +2,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegularExpression {
-        static Pattern pattern = Pattern.compile("^([a-z]{8,})$");
+        static Pattern pattern = Pattern.compile("^([A-z]?[a-z]{8,})$");
         public static void main(String[] args)
         {
-            Matcher matcher = pattern.matcher("akshayzagade");
+            Matcher matcher = pattern.matcher("Akshayzagade");
             if(matcher.matches())
                 System.out.println(true);
             else
