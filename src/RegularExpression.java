d51b11d4e68231cee96aa430aa426a1d56c0e8e8
akshayzagad <akshayzagade62@gmail.com.com>
2022-12-23 20:48:37 +0530

    Added;- Test Password Rule 8 And add atleast 1 Spaicial Symbol

--- src/RegularExpression.java
+++ src/RegularExpression.java
@@ -2,10 +2,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegularExpression {
-        static Pattern pattern = Pattern.compile("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}");
+        static Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=[^@#$%^&*+=]*[@#$%^&*+=][^@#$%^&*+=]*$).{8,}$");
         public static void main(String[] args)
         {
-            Matcher matcher = pattern.matcher("Akshayzagade62");
+            Matcher matcher = pattern.matcher("Akshayzagade62@");
             if(matcher.matches())
                 System.out.println(true);
             else
