86cf2d6edb612577daf54f20d96c2f898d88b94f
akshayzagad <akshayzagade62@gmail.com.com>
2022-12-23 20:01:58 +0530

    Added;- Test Gmail Address

--- src/RegularExpression.java
+++ src/RegularExpression.java
@@ -2,10 +2,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegularExpression {
-        static Pattern pattern = Pattern.compile("^([A-Z]{1}[a-z]{3,})$");
+        static Pattern pattern = Pattern.compile("^([a-zA-Z0-9.]+)@([a-zA-Z0-9.]+).([a-zA-Z]{2,5})$");
         public static void main(String[] args)
         {
-            Matcher matcher = pattern.matcher("Akshay");
+            Matcher matcher = pattern.matcher("abc.xyz@bl.co.in");
             if(matcher.matches())
                 System.out.println(true);
             else
