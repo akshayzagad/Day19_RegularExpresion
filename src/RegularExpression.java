5a0298cc12d8434b137f838c18af0723b0377aee
akshayzagad <akshayzagade62@gmail.com.com>
2022-12-23 20:07:35 +0530

    Added;- Test Phone Number

--- src/RegularExpression.java
+++ src/RegularExpression.java
@@ -2,10 +2,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegularExpression {
-        static Pattern pattern = Pattern.compile("^([a-zA-Z0-9.]+)@([a-zA-Z0-9.]+).([a-zA-Z]{2,5})$");
+        static Pattern pattern = Pattern.compile("^(^([0-9]{2})\s([0-9]{10})$)");
         public static void main(String[] args)
         {
-            Matcher matcher = pattern.matcher("abc.xyz@bl.co.in");
+            Matcher matcher = pattern.matcher("91 9739713842");
             if(matcher.matches())
                 System.out.println(true);
             else
